728d180a6db63740bbaa8c26eba12c0069bafb12
Arijit-123Banerjee <arijitbanerjee873@gmail.com>
2024-01-13 15:39:10 +0530

    added animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import { React, useState } from "react";
 import BeautyProduct from "./Components/HeroSectionPage/BeautyProduct";
 import NavBar from "./Components/HeroSectionPage/NavBar";
-
 import BackgroundIMG from "../src/backgroundIMG.jpg";
+import { motion } from "framer-motion";
 
 const App = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="h-screen bg-black">
-      <div
-        className="bg-cover bg-no-repeat bg-center h-screen bg-fixed relative "
-        style={{ backgroundImage: `url(${BackgroundIMG})` }}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
       >
-        <NavBar />
-      </div>
+        <motion.div
+          initial={{ x: -100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <div
+            className="bg bg-cover bg-no-repeat h-screen bg-fixed relative max-md:bg-[60%] bg-left "
+            style={{
+              backgroundImage: `url(${BackgroundIMG})`,
+            }}
+          >
+            <NavBar isOpen={isOpen} setOpen={setOpen} />
+          </div>
+        </motion.div>
+      </motion.div>
       <BeautyProduct />
     </div>
   );
--- src/Components/HeroSectionPage/NavBar.jsx
+++ src/Components/HeroSectionPage/NavBar.jsx
@@ -1,39 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 import MainSection from "./MainSection";
-import Hamburger from "hamburger-react";
+import { Rotate as Hamburger } from "hamburger-react";
+import { motion } from "framer-motion";
 
-const NavBar = () => {
-  const [isOpen, setOpen] = useState(false);
-  console.log(isOpen);
+const NavLink = ({ text }) => (
+  <li className="w-full p-14 cursor-pointer duration-300 hover:bg-[#407a7d] flex justify-center items-center hover:text-xl text-gray-400">
+    {text}
+  </li>
+);
 
-  return (
-    <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col">
-      <nav className="h-[10vh] max-md:flex max-md:justify-between max-md:items-center max-md:p-3 text-center pt-8">
-        <h1 className="text-white font-roboto max-md:text-xl text-3xl tracking-[0.4em]">
-          ShopNEXT
-        </h1>
-        <button className="max-md:hidden absolute right-0 top-1 p-4 text-white">
-          Toggle
-        </button>
-        <div className="text-white max-md:block hidden z-10">
-          <Hamburger size={21} toggled={isOpen} toggle={setOpen} />
-        </div>
-        <div className="menu max-md:hidden">
-          <ul className="list-none flex justify-center gap-14 text-white mt-6 text-md font-light">
-            <li>HOME</li>
-            <li>TESTIMONIALS</li>
-            <li>SHOP</li>
-            <li>ABOUT</li>
-            <li>HELP</li>
+const NavBar = ({ isOpen, setOpen }) => (
+  <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col">
+    <nav className="h-[10vh] max-md:flex max-md:justify-between max-md:items-center max-md:p-3 text-center pt-8">
+      <h1 className="text-white font-roboto max-md:text-xl text-3xl tracking-[0.4em]">
+        ShopNEXT
+      </h1>
+      <button className="max-md:hidden absolute right-0 top-1 p-4 text-white">
+        Toggle
+      </button>
+      <div className="text-white max-md:block hidden z-10">
+        <Hamburger size={21} toggled={isOpen} toggle={setOpen} />
+      </div>
+      <div className="menu max-md:hidden">
+        <ul className="list-none flex justify-center gap-14 text-white mt-6 text-md font-light">
+          {["HOME", "TESTIMONIALS", "SHOP", "ABOUT", "HELP"].map((text) => (
+            <li key={text}>{text}</li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+    {isOpen && (
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 0.96 }}>
+        <div className="h-screen w-full absolute top-0 z-0 bg-[#0a3639] flex flex-col items-center ">
+          {/* Updated z-index to ensure the navigation is on top of other elements */}
+          <ul className="h-screen w-full flex justify-center flex-col items-center gap-4">
+            {["HOME", "TESTIMONIALS", "SHOP", "ABOUT", "HELP"].map((text) => (
+              <NavLink key={text} text={text} />
+            ))}
           </ul>
         </div>
-      </nav>
-      {isOpen && (
-        <div className="h-screen w-[100%] bg-green-800 absolute top-0 z-0 "></div>
-      )}
-      <MainSection />
-    </div>
-  );
-};
+      </motion.div>
+    )}
+
+    <MainSection />
+  </div>
+);
 
 export default NavBar;
