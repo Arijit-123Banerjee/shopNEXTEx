e1cb65b9df3e20688d898cd223020a2059d54e16
Arijit-123Banerjee <arijitbanerjee873@gmail.com>
2024-01-13 11:47:55 +0530

    nothing change testing purpose

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import BeautyProduct from "./Components/HeroSectionPage/BeautyProduct";
 import NavBar from "./Components/HeroSectionPage/NavBar";
 
-import BackgroundIMG from "./photos/BackgroundIMG.jpg"; // Make sure to provide the correct file extension
+import BackgroundIMG from "./BackgroundIMG.jpg";
 
 const App = () => {
   return (
